70e30b7061ae1604dc31be1d2695bb22b68fea84
Asmik <jasmin_ayr@mail.ru>
2024-06-12 14:15:43 +0200

    ListNode created

---
+++ src/main/java/com/example/intersectionLinkedList/ListNode.java
@@ -0,0 +1,12 @@
+package com.example.intersectionLinkedList;
+
+public class ListNode {
+
+    int val;
+     ListNode next;
+     ListNode(int x) {
+      val = x;
+      next = null;
+      }
+}
+
