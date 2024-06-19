427f89bfb1bc891d9198d0f26e119ed67cbdfc09
Asmik <jasmin_ayr@mail.ru>
2024-06-19 19:10:50 +0200

    ListNode added

---
+++ src/main/java/com/example/deleteNNodesAfterMNodes/ListNode.java
@@ -0,0 +1,12 @@
+package com.example.deleteNNodesAfterMNodes;
+
+public class ListNode {
+
+    int val;
+     ListNode next;
+     ListNode(int x) {
+      val = x;
+      next = null;
+      }
+}
+
