6011ff0f18e54db5926bee4b4355544a1754257a
Asmik <jasmin_ayr@mail.ru>
2024-07-04 21:28:36 +0200

    SecondMaxNumber solved

---
+++ src/main/java/com/example/SecondMaxNumber.java
@@ -0,0 +1,31 @@
+package com.example;
+
+/**
+ * Write a Java program to find the second largest number in an array.
+ */
+
+public class SecondMaxNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {1,9,8,6,5,9,19};
+
+        System.out.println(find2ndLargestNum(nums));
+
+    }
+
+    public static int find2ndLargestNum(int[] nums){
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num: nums) {
+            if(num > first){
+                second = first;
+                first = num;
+            }else if(num > second && num != first){
+                second = num;
+            }
+        }
+
+        return second;
+    }
+}
