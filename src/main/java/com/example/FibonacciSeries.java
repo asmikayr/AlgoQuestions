df7c63a70de06869fed8024407e6c50b3c1ba332
Asmik <jasmin_ayr@mail.ru>
2024-07-04 23:39:33 +0200

    Fibonacci solved

---
+++ src/main/java/com/example/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package com.example;
+/*
+Write a Java program to print the Fibonacci series up to a given number.
+In mathematics, the Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones.
+ */
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        System.out.println(getFibonacciSeries(5));
+
+    }
+
+    public static int getFibonacciSeries(int n){
+        if (n <= 1) return 1;
+        return getFibonacciSeries(n-1)+getFibonacciSeries(n-2);
+    }
+}
