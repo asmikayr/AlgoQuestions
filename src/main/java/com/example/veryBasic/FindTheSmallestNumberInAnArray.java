59369ec598de4c557343e76000086ab56dc44a10
Asmik <jasmin_ayr@mail.ru>
2024-07-10 15:08:33 +0200

    FindTheSmallestNumberInArray done

---
+++ src/main/java/com/example/veryBasic/FindTheSmallestNumberInAnArray.java
@@ -0,0 +1,25 @@
+package com.example.veryBasic;
+/*
+Write a function to find the smallest number in an array.
+ */
+public class FindTheSmallestNumberInAnArray {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 7, 2, 8, 1};
+
+        System.out.println(findMinNumber(nums));
+    }
+
+    public static int findMinNumber(int[] nums){
+        int min = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if(min > nums[i]){
+                min = nums[i];
+            }
+
+        }
+        return min;
+    }
+
+}
