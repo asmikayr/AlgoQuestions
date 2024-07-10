4c04b7fe7ded784295c040534dd280ace70fa67f
Asmik <jasmin_ayr@mail.ru>
2024-07-10 20:33:59 +0200

    FindTheSumOfAllNums is done

--- src/main/java/com/example/veryBasic/FindTheSumOfAllNums.java
+++ src/main/java/com/example/veryBasic/FindTheSumOfAllNums.java
@@ -1,5 +1,11 @@
 package com.example.veryBasic;
 
+/*
+Day 3: Sum of Array Elements
+Problem Description:
+Write a function to find the sum of all elements in an array.
+ */
+
 public class FindTheSumOfAllNums {
 
     public static void main(String[] args) {
