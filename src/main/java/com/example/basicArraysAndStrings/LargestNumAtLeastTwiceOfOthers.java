37e872058aa2ea58b6eb8ef232313e28f040d9f4
Asmik <jasmin_ayr@mail.ru>
2024-07-14 23:46:33 +0200

    LargestNumAtLeastTwiceOfOthers set up

---
+++ src/main/java/com/example/basicArraysAndStrings/LargestNumAtLeastTwiceOfOthers.java
@@ -0,0 +1,10 @@
+package com.example.basicArraysAndStrings;
+/*
+Problem Description:
+Given an integer array nums, find whether the largest element in the array is at least twice as much
+as every other number in the array. If it is, return the index of the largest element, otherwise,
+return -1.
+ */
+
+public class LargestNumAtLeastTwiceOfOthers {
+}
