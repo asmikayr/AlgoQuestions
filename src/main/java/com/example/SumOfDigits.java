023acf65ec94b471a7acf1c816ecaad80729fe6b
Asmik <jasmin_ayr@mail.ru>
2024-07-15 23:12:35 +0200

    SumOfDigits set

---
+++ src/main/java/com/example/SumOfDigits.java
@@ -0,0 +1,24 @@
+package com.example;
+/*
+Question-2 Sum of Digits
+Given n, take the sum of the digits of n, if that value has more than one digit, continue reducing in this way until
+a single-digit number is produced.
+Examples:
+16 🡪 1 + 6 = 7
+942 🡪 9 + 4 + 2 = 15 🡪 1 + 5 🡪 6
+132189 🡪 1 + 3 + 2 + 1 + 8 + 9 + 24 🡪 2 + 4 = 6
+493193 🡪 4 + 9 + 3 + 1 + 9 + 3 = 29 🡪 2 + 9 = 11 🡪 1 + 1 = 2
+Constraints: The input will be a non-negative integer.
+
+ */
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getSumOfNum(){
+
+    }
+}
