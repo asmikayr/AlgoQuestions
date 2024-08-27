6690f4dcc6b30cf0cdb3424f527157bc250233e0
Asmik <jasmin_ayr@mail.ru>
2024-08-27 11:30:46 +0200

    MergeSortedList second solution added

--- src/main/java/com/example/MergeSortedList.java
+++ src/main/java/com/example/MergeSortedList.java
@@ -25,5 +25,31 @@ public class MergeSortedList {
         return dummy.next;
     }
 
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+
+        ListNode temp = new ListNode(0); // Dummy node to start the merged list
+        ListNode current = temp;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // At this point, at least one of the lists is null
+        if(list1 != null) {
+            current.next = list1; // Attach the remaining list1 if list2 is exhausted
+        } else {
+            current.next = list2; // Attach the remaining list2 if list1 is exhausted
+        }
+
+        return temp.next; // Return the merged list, skipping the dummy node
+    }
+
 
 }
