acf1dc0bf3516a08002acba13dafc56c871d9db2
Asmik <jasmin_ayr@mail.ru>
2024-08-27 23:42:27 +0200

    DI example added

---
+++ src/main/java/com/example/reviewMock/DIexample.java
@@ -0,0 +1,10 @@
+package com.example.reviewMock;
+
+public class DIexample {
+
+    private final ArrayListMock arrayListMock;
+
+    public DIexample(ArrayListMock arrayListMock) {
+        this.arrayListMock = arrayListMock;
+    }
+}
