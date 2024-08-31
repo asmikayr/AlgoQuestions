644bf57c15e8bf221087692bf92509d3e5626c7d
Asmik <jasmin_ayr@mail.ru>
2024-09-01 00:36:24 +0200

    Sorting and Searching: BinarySearch test cases added

--- src/main/java/com/algoquestionsAll/sortingAndSearching/BinarySearch.java
+++ src/main/java/com/algoquestionsAll/sortingAndSearching/BinarySearch.java
@@ -7,21 +7,33 @@ return the index of target if it is present. Otherwise, return -1.
  */
 
 public class BinarySearch {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
+        // Calculate the middle index
         int left = 0, right = nums.length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                return mid;
+                return mid; // Target found
             } else if (nums[mid] < target) {
-                left = mid + 1;
+                left = mid + 1; // Search the right half
             } else {
-                right = mid - 1;
+                right = mid - 1; // Search the left half
             }
         }
 
-        return -1;
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        // Test case 1: nums = [-1, 0, 3, 5, 9, 12], target = 9
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println("Index of target: " + search(nums, target)); // Output: 4
+
+        // Test case 2: nums = [-1, 0, 3, 5, 9, 12], target = 2
+        target = 2;
+        System.out.println("Index of target: " + search(nums, target)); // Output: -1
     }
 }
 
